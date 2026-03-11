b58e579e3ede683c44281131c503caae72a03967
manto187 <asadameen050@gmail.com>
2026-03-11 15:05:15 +0500

    add node at end

---
+++ Week 4/singly linked lists/task2.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+class LinkedList{
+private:
+    Node* head;
+
+public:
+    LinkedList(){
+        head=NULL;
+    }
+
+    void addAtEnd(int value){
+
+        Node* newNode = new Node();
+        newNode->data=value;
+        newNode->next=NULL;
+
+        if(head==NULL){
+            head=newNode;
+        }
+        else{
+            Node* temp=head;
+
+            while(temp->next!=NULL){
+                temp=temp->next;
+            }
+
+            temp->next=newNode;
+        }
+    }
+
+    void display(){
+
+        Node* temp=head;
+
+        while(temp!=NULL){
+            cout<<temp->data<<" -> ";
+            temp=temp->next;
+        }
+
+        cout<<"NULL\n";
+    }
+};
+
+int main(){
+
+    LinkedList list;
+
+    list.addAtEnd(10);
+    list.addAtEnd(20);
+    list.addAtEnd(30);
+
+    list.display();
+
+    return 0;
+}
