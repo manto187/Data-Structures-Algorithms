ac622fbe88516842d82ee1ebb3f499685404a869
manto187 <asadameen050@gmail.com>
2026-03-11 15:02:22 +0500

    adding node at start

---
+++ Week 4/singly linked lists/AddNodeAtStart.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class SinglyLinkedList {
+private:
+    Node* head;
+
+public:
+    SinglyLinkedList() {
+        head = NULL;
+    }
+
+    void addAtStart(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void display() {
+        Node* temp = head;
+        while(temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+};
+
+int main() {
+
+    SinglyLinkedList list;
+
+    list.addAtStart(10);
+    list.addAtStart(20);
+    list.addAtStart(30);
+
+    list.display();
+
+    return 0;
+}
