96f8303e28e36e7d4a9f9ebec56b59c46bd180a6
manto187 <asadameen050@gmail.com>
2026-04-05 02:07:22 +0500

    get first and last occurence of sorted array

---
+++ Week 5/Binary Search/task2_modify.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int BinarySearch(int A[], int n, int x,bool searchFirst){
+    int low = 0, high = n-1, result=-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(A[mid]==x){
+            result = mid;
+            if(searchFirst)
+             high = mid-1;
+            else 
+            low = mid+1;
+
+        }
+        else if(x<A[mid]) high = mid-1;
+        else low = mid+1;
+    }
+    return result;
+
+}
